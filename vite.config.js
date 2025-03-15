1491bc81ef8f3381c2d614d313c31102fbdd740a
acpopper <acpopper@uc.cl>
2025-03-15 01:44:26 -0300

    added gh pages deployment for trying on other devices

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import vue from "@vitejs/plugin-vue";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: "/small-weather-app",
   server: {
     port: 3000,
     hmr: {
